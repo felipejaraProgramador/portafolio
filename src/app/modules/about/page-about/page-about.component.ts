0bad5f134b6f492415c5427ad76e143623fbdd85
Felipe Jara <fjara9613@gmail.com>
2023-05-18 23:21:53 -0400

    fix age in about

--- src/app/modules/about/page-about/page-about.component.ts
+++ src/app/modules/about/page-about/page-about.component.ts
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAboutComponent implements OnInit {
 
+  age: number = 404;
+
+  dayBirth: number = 13;
+  monthBirth: number = 5;
+  yearBirth: number = 1996;
+
+  date = new Date();
 
   constructor() {
+    this.age = this.getAge();
   }
 
   async ngOnInit() {
+  }
+
+  getAge(): number{
+    let auxAge = this.date.getFullYear() - this.yearBirth;
+
+    if((this.date.getMonth()+1) < this.monthBirth){      
+      return auxAge -= 1;
+    }else if((this.date.getMonth()+1) == this.monthBirth && this.date.getDate() < this.dayBirth){
+      return auxAge -= 1;
+    }else{
+      return auxAge;
+    }    
+
   }
 
 
